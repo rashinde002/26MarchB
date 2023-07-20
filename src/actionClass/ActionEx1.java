16a238b4951a05eaf45d79ac6a95c8a34fcabec5
Dev <balsarafdevanad>
2023-07-20 14:33:21 +0530

    [Dev] comment for drivers

--- src/actionClass/ActionEx1.java
+++ src/actionClass/ActionEx1.java
@@ -10,8 +10,8 @@ public class ActionEx1 {
 	public static void main(String[] args) throws InterruptedException 
 	{
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver.exe");	
-	WebDriver driver= new ChromeDriver();
-	driver.manage().window().maximize();
+	WebDriver driver= new ChromeDriver(); //Open URL in the browser
+	driver.manage().window().maximize(); // maximize the browser
 	driver.get("https://demo.guru99.com/test/simple_context_menu.html");
 	driver.findElement(By.linkText("Selenium"));
 	Thread.sleep(1000);
